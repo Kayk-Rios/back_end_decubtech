f0e723df5a7e69ed308b960adb8eb21a60cd695f
Kayk-Rios <manjac688@gmail.com>
2024-11-04 18:10:18 -0300

    feat:criaçaoUsuario/LoginUsuario

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('register')
+    async register(@Body() body: { username: string; password: string }) {
+      return this.usersService.createUser(body);
+    }
+  
+    @Post('login')
+    async login(@Body() body: { username: string; password: string }) {
+      const user = await this.usersService.login(body.username, body.password);
+      if (!user) {
+        return { message: 'Invalid credentials' };
+      }
+      return user;
+    }
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface CreateUserData {
+    username: string;
+    password: string;
+  }
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async createUser(data: CreateUserData) {
+      return this.prisma.user.create({ 
+        data: {
+          username: data.username,
+          password: data.password
+        } 
+      });
+    }
+  
+    async login(username: string, password: string) {
+      return this.prisma.user.findFirst({
+        where: {
+          username,
+          password,
+        },
+      });
+    }
+
+}
