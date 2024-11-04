eeae5c47bd5410b34416cec87518610b30db4755
Kayk-Rios <manjac688@gmail.com>
2024-11-04 18:45:45 -0300

    feat:criaçaoSetoresAssociadoUsuario

--- src/setores/setores.module.ts
+++ src/setores/setores.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SetoresService } from './setores.service';
 import { SetoresController } from './setores.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [SetoresService],
+  providers: [SetoresService, PrismaService],
   controllers: [SetoresController]
 })
 export class SetoresModule {}
