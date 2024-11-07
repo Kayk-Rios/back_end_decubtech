758ea5687bef7579bfde44a8eda4bd9403544efb
Kayk-Rios <manjac688@gmail.com>
2024-11-07 10:36:10 -0300

    feat:logicaLeitos

--- src/leitos/leitos.module.ts
+++ src/leitos/leitos.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LeitosService } from './leitos.service';
 import { LeitosController } from './leitos.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [LeitosService],
+  providers: [LeitosService, PrismaService],
   controllers: [LeitosController]
 })
 export class LeitosModule {}
